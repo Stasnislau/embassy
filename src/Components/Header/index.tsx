355d0e53bea9e1c2e9fbc08bef1d14481a2611fe
Stanislau Ryzhkou <stanislaurv@gmail.com>
2022-12-30 02:20:42 +0100

    initial commit

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="header-left-side">
+            left side
+        </div>
+        <div className="header-center">
+            center
+        </div>
+        <div className="header-right-side">
+            right side
+        </div>
+      </div>
+    </header>
+  );
+};
