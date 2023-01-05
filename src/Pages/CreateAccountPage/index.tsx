10a81eb7b6a716005a935b7d54279e1604824ba9
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-01-05 22:14:27 +0100

    formik refactoring

--- src/Pages/CreateAccountPage/index.tsx
+++ src/Pages/CreateAccountPage/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./index.css";
 import { useState } from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup'
 
 interface values {
   name: string;
@@ -25,108 +26,90 @@ const CreateAccount = () => {
       setPasswordsMatch(false);
     }
   };
-  const validate = (values: values) => {
-    const errors = {};
-    if (!values.name) {
-      formik.errors.name = "Required";
-    }
-    if (!values.surname) {
-      formik.errors.surname = "Required";
-    }
-    if (!values.email) {
-      formik.errors.email = "Required";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      formik.errors.email = "Invalid email address";
-    }
-    return errors;
-  };
-
+  // const validate = (values: values) => {
+  //   const errors = {};
+  //   if (!values.name) {
+  //     formik.errors.name = "Required";
+  //   }
+  //   if (!values.surname) {
+  //     formik.errors.surname = "Required";
+  //   }
+  //   if (!values.email) {
+  //     formik.errors.email = "Required";
+  //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+  //     formik.errors.email = "Invalid email address";
+  //   }
+  //   return errors;
+  // };
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Required'),
+    surname: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email address').required('Required'),
+  }) 
   const [passwordsMatch, setPasswordsMatch] = useState(true);
-  const formik = useFormik({
-    initialValues: initialValues,
-    onSubmit: onSubmit,
-    validate: validate,
-  });
   return (
     <div className="create-account-container">
       <div className="create-account-form-container">
-        <form onSubmit={formik.handleSubmit}>
-          <div className="form-control">
-            <label htmlFor="name">Name</label>
-            <input
-              id="name"
-              name="name"
-              type="text"
-              onBlur={formik.handleBlur}
-              className="input-field"
-              onChange={formik.handleChange}
-              value={formik.values.name}
-            />
-            {formik.errors.name ? (
-              <div className="error-text">{formik.errors.name}</div>
-            ) : null}
-          </div>
-          <div className="form-control">
-            <label htmlFor="surname">Surname</label>
-            <input
-              id="surname"
-              name="surname"
-              type="text"
-              className="input-field"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              value={formik.values.surname}
-            />
-            {formik.errors.surname ? (
-              <div className="error-text">{formik.errors.surname}</div>
-            ) : null}
-          </div>
-          <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              className="input-field"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
-          </div>
-          {formik.errors.email ? (
-            <div className="error-text">{formik.errors.email}</div>
-          ) : null}
-          <div className="form-control">
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              className="input-field"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              id="confirmPassword"
-              name="confirmPassword"
-              type="password"
-              className="input-field"
-              onChange={formik.handleChange}
-              value={formik.values.confirmPassword}
-            />
-          </div>
-          <div className="buttons-container">
-            <button className="submit-button" type="submit">
-              Sign Up
-            </button>
-            <button className="back-button" type="button">
-              Back
-            </button>
-          </div>
-        </form>
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+          <Form>
+            <div className="form-control">
+              <label htmlFor="name">Name</label>
+              <Field
+                id="name"
+                name="name"
+                type="text"
+                className="input-field"
+              />
+              <ErrorMessage name="name"/>
+            </div>
+            <div className="form-control">
+              <label htmlFor="surname">Surname</label>
+              <Field
+                id="surname"
+                name="surname"
+                type="text"
+                className="input-field"
+              />
+              <ErrorMessage name="surname"/>
+            </div>
+            <div className="form-control">
+              <label htmlFor="email">Email</label>
+              <Field
+                id="email"
+                name="email"
+                type="email"
+                className="input-field"
+              />
+            </div>
+            <ErrorMessage name="email"/>
+            <div className="form-control">
+              <label htmlFor="password">Password</label>
+              <Field
+                id="password"
+                name="password"
+                type="password"
+                className="input-field"
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <Field
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                className="input-field"
+              />
+            </div>
+            <div className="buttons-container">
+              <button className="submit-button" type="submit">
+                Sign Up
+              </button>
+              <button className="back-button" type="button">
+                Back
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </div>
       {passwordsMatch ? null : (
         <div className="error-text">Passwords do not match</div>
