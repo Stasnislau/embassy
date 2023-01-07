438ca0f6cbaeb6237b78e9f8c76c08a1ee8c3872
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-01-07 20:57:42 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "./App.css";
 import LoginPage from "../src/Pages/LoginPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "../src/Components/Footer";
 import CreateAccountPage from "../src/Pages/CreateAccountPage";
 // import Header from "../src/Components/Header";
 function App() {
   return (
-    <div className="app">
-      {/* <Header/> */}
-      {/* <LoginPage /> */}
-      <CreateAccountPage />
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        {/* <Header/> */}
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />}></Route>
+          <Route
+            exact
+            path="/create-account"
+            element={<CreateAccountPage />}
+          ></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
