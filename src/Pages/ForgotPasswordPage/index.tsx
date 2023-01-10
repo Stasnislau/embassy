185a31185f3d019a29c2e9b4acecdfabf274d704
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-01-10 23:34:29 +0100

    improved forgot password pge

--- src/Pages/ForgotPasswordPage/index.tsx
+++ src/Pages/ForgotPasswordPage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.scss";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import TextError from "../../Components/TextError";
 import * as Yup from "yup";
 
 const ForgotPassword: React.FC = () => {
@@ -19,56 +20,99 @@ const ForgotPassword: React.FC = () => {
   const handleSubmit = (values: any) => {
     console.log(values);
   };
+  const initialValues = {
+    name: "",
+    surname: "",
+    email: "",
+    newPassword: "",
+    confirmPassword: "",
+  };
 
   return (
     <div className="forgot-password-container">
-      <Formik
-        initialValues={{
-          name: "",
-          surname: "",
-          email: "",
-          newPassword: "",
-          confirmPassword: "",
-        }}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {() => (
+      <div className="forgot-password-form-container">
+        <Formik
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+          initialValues={initialValues}
+        >
           <Form>
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <Field type="text" name="name" className="form-control" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="surname">Surname:</label>
-              <Field type="text" name="surname" className="form-control" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <Field type="email" name="email" className="form-control" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="newPassword">New Password:</label>
+            <h1>Forgot Password</h1>
+            <div className="form-control">
+              <label htmlFor="email">Email</label>
               <Field
-                type="password"
+                className="input-field"
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email for your account"
+              />
+
+              <ErrorMessage name="email" component={TextError} />
+            </div>
+
+            <div className="form-control">
+              <label htmlFor="name">Name</label>
+              <Field
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Enter your name"
+                className="input-field"
+              />
+              <ErrorMessage name="name" component={TextError} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="surname">Surname</label>
+              <Field
+                id="surname"
+                name="surname"
+                type="text"
+                placeholder="Enter your surname"
+                className="input-field"
+              />
+              <ErrorMessage name="surname" component={TextError} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="newPassword">New Password</label>
+              <Field
+                id="newPassword"
                 name="newPassword"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="confirmPassword">Confirm Password:</label>
-              <Field
                 type="password"
-                name="confirmPassword"
-                className="form-control"
+                className="input-field"
+                placeholder="Enter your new password"
               />
+              <ErrorMessage name="newPassword" component={TextError} />
             </div>
-            <button type="submit" className="btn btn-primary">
-              Reset Password
-            </button>
+            <div className="form-control">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <Field
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                className="input-field"
+                placeholder="Confirm your new password"
+              />
+              <ErrorMessage name="confirmPassword" component={TextError} />
+            </div>
+
+            <div className="buttons-container">
+              <button type="submit" className="submit-button">
+                Change Password
+              </button>
+              <button
+                className="back-button"
+                type="button"
+                onClick={() => {
+                  window.location.href = "/login";
+                }}
+              >
+                Back
+              </button>
+            </div>
           </Form>
-        )}
-      </Formik>
+        </Formik>
+      </div>
     </div>
   );
 };
