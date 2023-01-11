cfb7ec43c9ba8da77bc7d5ff2d44297bab9aa4c1
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-01-11 23:40:24 +0100

    finished forgot password page

--- src/Pages/ForgotPasswordPage/index.tsx
+++ src/Pages/ForgotPasswordPage/index.tsx
@@ -17,7 +17,7 @@ const ForgotPassword: React.FC = () => {
       .oneOf([Yup.ref("newPassword"), null], "Passwords must match"),
   });
 
-  const handleSubmit = (values: any) => {
+  const onSubmit = (values: any) => {
     console.log(values);
   };
   const initialValues = {
@@ -31,71 +31,72 @@ const ForgotPassword: React.FC = () => {
   return (
     <div className="forgot-password-container">
       <div className="forgot-password-form-container">
+        <h1>Forgot Password</h1>
         <Formik
           validationSchema={validationSchema}
-          onSubmit={handleSubmit}
+          onSubmit={onSubmit}
           initialValues={initialValues}
         >
           <Form>
-            <h1>Forgot Password</h1>
-            <div className="form-control">
-              <label htmlFor="email">Email</label>
-              <Field
-                className="input-field"
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Email for your account"
-              />
+            <div className="inputs-container">
+              <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <Field
+                  className="input-field"
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Email for your account"
+                />
 
-              <ErrorMessage name="email" component={TextError} />
-            </div>
+                <ErrorMessage name="email" component={TextError} />
+              </div>
 
-            <div className="form-control">
-              <label htmlFor="name">Name</label>
-              <Field
-                id="name"
-                name="name"
-                type="text"
-                placeholder="Enter your name"
-                className="input-field"
-              />
-              <ErrorMessage name="name" component={TextError} />
+              <div className="form-control">
+                <label htmlFor="name">Name</label>
+                <Field
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Enter your name"
+                  className="input-field"
+                />
+                <ErrorMessage name="name" component={TextError} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="surname">Surname</label>
+                <Field
+                  id="surname"
+                  name="surname"
+                  type="text"
+                  placeholder="Enter your surname"
+                  className="input-field"
+                />
+                <ErrorMessage name="surname" component={TextError} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="newPassword">New Password</label>
+                <Field
+                  id="newPassword"
+                  name="newPassword"
+                  type="password"
+                  className="input-field"
+                  placeholder="Enter your new password"
+                />
+                <ErrorMessage name="newPassword" component={TextError} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <Field
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  className="input-field"
+                  placeholder="Confirm your new password"
+                />
+                <ErrorMessage name="confirmPassword" component={TextError} />
+              </div>
             </div>
-            <div className="form-control">
-              <label htmlFor="surname">Surname</label>
-              <Field
-                id="surname"
-                name="surname"
-                type="text"
-                placeholder="Enter your surname"
-                className="input-field"
-              />
-              <ErrorMessage name="surname" component={TextError} />
-            </div>
-            <div className="form-control">
-              <label htmlFor="newPassword">New Password</label>
-              <Field
-                id="newPassword"
-                name="newPassword"
-                type="password"
-                className="input-field"
-                placeholder="Enter your new password"
-              />
-              <ErrorMessage name="newPassword" component={TextError} />
-            </div>
-            <div className="form-control">
-              <label htmlFor="confirmPassword">Confirm Password</label>
-              <Field
-                id="confirmPassword"
-                name="confirmPassword"
-                type="password"
-                className="input-field"
-                placeholder="Confirm your new password"
-              />
-              <ErrorMessage name="confirmPassword" component={TextError} />
-            </div>
-
             <div className="buttons-container">
               <button type="submit" className="submit-button">
                 Change Password
