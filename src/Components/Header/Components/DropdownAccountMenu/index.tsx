4468bb8f94e0eec3ebea45b297157bb2de6f67a4
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-01-22 13:34:49 +0100

    almost finished accountMenu

--- src/Components/Header/Components/DropdownAccountMenu/index.tsx
+++ src/Components/Header/Components/DropdownAccountMenu/index.tsx
@@ -4,33 +4,41 @@ import React, { useState } from "react";
 
 import AccountIcon from "../../../../Pictures/accountIcon.svg";
 import DropdownDown from "../../../../Pictures/arrowDown.svg";
-import DropdownUp from "../../../../Pictures/arrowUp.svg";
 
 const DropdownAccountMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
-      <div className="dropdown-account-button-container">
-        <img src={AccountIcon} alt="Account Logo" />
+    <div className="dropdown-account-button-container">
+      <div className="trigger-button-container">
+        <img src={AccountIcon} className="account-logo" alt="Account Logo" />
         <button
           className="dropdown-account-button"
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? (
-            <img src={DropdownUp} className="arrow" alt="Dropdown Up" />
+            <img
+              src={DropdownDown}
+              className="arrow arrow-up"
+              alt="Dropdown Up"
+            />
           ) : (
-            <img src={DropdownDown} className="arrow" alt="Dropdown Down" />
+            <img
+              src={DropdownDown}
+              className="arrow arrow-down"
+              alt="Dropdown Down"
+            />
           )}
         </button>
+
+        {isOpen && (
+          <ul className="dropdown-account-menu">
+            <li className="dropdown-account-menu-item">Option 1</li>
+            <li className="dropdown-account-menu-item">Option 2</li>
+            <li className="dropdown-account-menu-item">Option 3</li>
+          </ul>
+        )}
       </div>
-      {isOpen && (
-        <ul className="dropdown-account-menu">
-          <li className="dropdown-account-menu-item">Option 1</li>
-          <li className="dropdown-account-menu-item">Option 2</li>
-          <li className="dropdown-account-menu-item">Option 3</li>
-        </ul>
-      )}
     </div>
   );
 };
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,12 +2,15 @@ import "./index.scss";
 
 import DropdownAccountMenu from "./Components/DropdownAccountMenu";
 import React from "react";
+import vejshnoriaLogo from "../../Pictures/vejshnoriaLogo.svg";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <div className="left-container">{/* add something here */}</div>
+        <div className="left-container">
+          {/* <img src={vejshnoriaLogo} className="logo" alt="Vejshnoria Logo" /> */}
+          </div>
         <div className="right-container">
           <div className="quick-access-menus-container">
               <DropdownAccountMenu />
