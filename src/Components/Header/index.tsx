3d173a4cae1909a9fbfaf7a8bdb66564b91a38ab
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-01-24 23:43:08 +0100

    small changes with logo

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,9 +1,9 @@
 import "./index.scss";
 
 import DropdownAccountMenu from "./Components/DropdownAccountMenu";
-import MinistryLogo from "../../../Pictures/ministryLogo.png";
+import MinistryLogo from "./../../Pictures/ministryLogo.png"
 import React from "react";
-import ResidencePermitDropdown from "./Components/ResidancePermitDropdown";
+import ResidencePermitDropdown from "./Components/ResidencePermitDropdown";
 import VisaDropdownMenu from "./Components/VisaDropdown";
 
 const Header = () => {
@@ -11,7 +11,7 @@ const Header = () => {
     <header className="header">
       <div className="header-container">
         <div className="left-container">
-          <img src={MinistryLogo} alt="Ministry Logo" />
+          <img src={MinistryLogo} className="ministry-logo" alt="Ministry Logo" />
         </div>
         <div className="right-container">
           <DropdownAccountMenu />
