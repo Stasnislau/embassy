3e87418682a898a1b23423fa877673a3bd3b10c3
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-02-05 16:33:31 +0100

    implementing the majority of features of visaAplic

--- src/Pages/VisaApplication/index.tsx
+++ src/Pages/VisaApplication/index.tsx
@@ -4,8 +4,10 @@ import * as Yup from "yup";
 
 import { ErrorMessage, Field, Form, Formik } from "formik";
 
+import Header from "../../Components/Header";
 import React from "react";
 import TextError from "../../Components/TextError";
+import { useNavigate } from "react-router-dom";
 
 interface VisaValues {
   firstName: string;
@@ -60,18 +62,18 @@ const VisaApplication = () => {
     address: Yup.string().required("Required"),
     city: Yup.string().required("Required"),
     country: Yup.string().required("Required"),
-    state: Yup.string().required("Required"),
     zip: Yup.string().required("Required"),
 
     visaType: Yup.string().required("Required"),
-    visaDuration: Yup.string().required("Required"),
     visaDate: Yup.string().required("Required"),
   });
   const onSubmit = (values: VisaValues) => {
     console.log(values);
   };
+  const navigate = useNavigate();
   return (
-    <div className="visa-application">
+    <div className="visa-application-container">
+      <Header />
       <div className="visa-application-form-container">
         <h1>Visa Application</h1>
         <Formik
@@ -81,7 +83,7 @@ const VisaApplication = () => {
         >
           <Form>
             <div className="inputs-container">
-              <div className="name-surname-container">
+              <div className="several-fields-container">
                 <div className="form-control">
                   <label htmlFor="firstName">First Name</label>
                   <Field
@@ -89,7 +91,7 @@ const VisaApplication = () => {
                     name="firstName"
                     type="text"
                     placeholder="First Name"
-                    className="first-name-field"
+                    className="first-name-field input-field"
                   />
                   <ErrorMessage name="firstName" component={TextError} />
                 </div>
@@ -100,12 +102,12 @@ const VisaApplication = () => {
                     name="lastName"
                     type="text"
                     placeholder="Last Name"
-                    className="last-name-field"
+                    className="last-name-field input-field"
                   />
                   <ErrorMessage name="lastName" component={TextError} />
                 </div>
               </div>
-              <div className="phone-email-container">
+              <div className="several-fields-container">
                 <div className="form-control">
                   <label htmlFor="email">Email</label>
                   <Field
@@ -113,7 +115,7 @@ const VisaApplication = () => {
                     name="email"
                     type="email"
                     placeholder="Email"
-                    className="email-field"
+                    className="email-field input-field"
                   />
                   <ErrorMessage name="email" component={TextError} />
                 </div>
@@ -124,110 +126,153 @@ const VisaApplication = () => {
                     name="phone"
                     type="text"
                     placeholder="Phone"
-                    className="phone-field"
+                    className="phone-field input-field"
                   />
                   <ErrorMessage name="phone" component={TextError} />
                 </div>
-                <div className="passport-fields-container">
-                  <div className="form-control">
-                    <label htmlFor="passportNumber">Passport Number</label>
-                    <Field
-                      id="passportNumber"
-                      name="passportNumber"
-                      type="text"
-                      placeholder="Passport Number"
-                      className="passport-number-field"
-                    />
-                    <ErrorMessage name="passportNumber" component={TextError} />
-                  </div>
-                  <div className="form-control">
-                    <label htmlFor="passportExpiration">
-                      Passport Expiration
-                    </label>
-                    <Field
-                      id="passportExpiration"
-                      name="passportExpiration"
-                      type="text"
-                      placeholder="Passport Expiration"
-                      className="passport-expiration-field"
-                    />
-                    <ErrorMessage
-                      name="passportExpiration"
-                      component={TextError}
-                    />
+              </div>
 
-                    <div className="form-control">
-                      <label htmlFor="passportCountry">Passport Country</label>
-                      <Field
-                        id="passportCountry"
-                        name="passportCountry"
-                        type="text"
-                        placeholder="Passport Country"
-                        className="passport-country-field"
-                      />
-                      <ErrorMessage
-                        name="passportCountry"
-                        component={TextError}
-                      />
-                    </div>
-                  </div>
-                  <div className="address-container">
-                    <div className="form-control">
-                      <label htmlFor="address">Country</label>
-                      <Field
-                        id="address"
-                        name="address"
-                        type="text"
-                        placeholder="Address"
-                        className="address-field"
-                      />
-                      <ErrorMessage name="address" component={TextError} />
-                    </div>
-                    <div className="form-control">
-                      <label htmlFor="city">City</label>
-                      <Field
-                        id="city"
-                        name="city"
-                        type="text"
-                        placeholder="City"
-                        className="city-field"
-                      />
-                      <ErrorMessage name="city" component={TextError} />
-                    </div>
-                    <div className="form-control">
-                      <label htmlFor="state">State</label>
-                      <Field
-                        id="state"
-                        name="state"
-                        type="text"
-                        placeholder="State"
-                        className="state-field"
-                      />
-                      <ErrorMessage name="state" component={TextError} />
-                    </div>
-                    <div className="form-control">
-                      <label htmlFor="zip">Zip</label>
-                      <Field
-                        id="zip"
-                        name="zip"
-                        type="text"
-                        placeholder="Zip"
-                        className="zip-field"
-                      />
-                      <ErrorMessage name="zip" component={TextError} />
-                      <label htmlFor="visaType">Visa Type</label>
-                      <select name="visaType" id="visaType">
-                        <option value="C Tourist">C Tourist</option>
-                        <option value="C Transit">C Transit</option>
-                        <option value="D Business">D Business</option>
-                        <option value="D Student">D Student</option>
-                        <option value="D Work">D Work</option>
-                        <option value="D Refugee">D Transit</option>
-                      </select>
-                    </div>
-                  </div>
+              <div className="form-control">
+                <label htmlFor="passportNumber">Passport Number</label>
+                <Field
+                  id="passportNumber"
+                  name="passportNumber"
+                  type="text"
+                  placeholder="Passport Number"
+                  className="passport-number-field input-field"
+                />
+                <ErrorMessage name="passportNumber" component={TextError} />
+              </div>
+              <div className="several-fields-container">
+                <div className="form-control">
+                  <label htmlFor="passportExpiration">
+                    Passport Expiration
+                  </label>
+                  <Field
+                    id="passportExpiration"
+                    name="passportExpiration"
+                    type="text"
+                    placeholder="Passport Expiration"
+                    className="passport-expiration-field input-field"
+                  />
+                  <ErrorMessage
+                    name="passportExpiration"
+                    component={TextError}
+                  />
+                </div>
+                <div className="form-control">
+                  <label htmlFor="passportCountry">Country of issue</label>
+                  <Field
+                    id="passportCountry"
+                    name="passportCountry"
+                    type="text"
+                    placeholder="Country of issue"
+                    className="passport-country-field input-field"
+                  />
+                  <ErrorMessage name="passportCountry" component={TextError} />
                 </div>
               </div>
+              <div className="address-container">
+                <div className="form-control">
+                  <label htmlFor="address">Address</label>
+                  <Field
+                    id="address"
+                    name="address"
+                    type="text"
+                    placeholder="Address"
+                    className="address-field input-field"
+                  />
+                  <ErrorMessage name="address" component={TextError} />
+                </div>
+              </div>
+              <div className="several-fields-container">
+                <div className="form-control">
+                  <label htmlFor="country">Country</label>
+                  <Field
+                    id="country"
+                    name="country"
+                    type="text"
+                    placeholder="Country"
+                    className="country-field input-field"
+                  />
+                  <ErrorMessage name="country" component={TextError} />
+                </div>
+                <div className="form-control">
+                  <label htmlFor="city">City</label>
+                  <Field
+                    id="city"
+                    name="city"
+                    type="text"
+                    placeholder="City"
+                    className="city-field input-field"
+                  />
+                  <ErrorMessage name="city" component={TextError} />
+                </div>
+
+                <div className="form-control">
+                  <label htmlFor="zip">Zip</label>
+                  <Field
+                    id="zip"
+                    name="zip"
+                    type="text"
+                    placeholder="Zip"
+                    className="zip-field input-field"
+                  />
+                  <ErrorMessage name="zip" component={TextError} />
+                </div>
+              </div>
+              <div className="several-fields-container">
+                <div className="form-control">
+                  <label htmlFor="visaDate">Visa valid from</label>
+                  <Field
+                    id="visaDate"
+                    name="visaDate"
+                    type="text"
+                    placeholder="Visa valid from"
+                    className="visa-date-field input-field"
+                  />
+                  <ErrorMessage name="visaDate" component={TextError} />
+                </div>
+                <div className="form-control">
+                  <label htmlFor="visaType">Visa Type</label>
+                  <select className="input-field" name="visaType" id="visaType">
+                    <option value="C Tourist">C Tourist</option>
+                    <option value="C Transit">C Transit</option>
+                    <option value="D Business">D Business</option>
+                    <option value="D Student">D Student</option>
+                    <option value="D Work">D Work</option>
+                    <option value="D Refugee">D Transit</option>
+                  </select>
+                </div>
+                <div className="form-control">
+                  <label htmlFor="visaDuration">Visa Duration</label>
+                  <select
+                    className="input-field"
+                    name="visaDuration"
+                    id="visaDuration"
+                  >
+                    <option value="1 Month">1 Month</option>
+                    <option value="3 Months">3 Months</option>
+                    <option value="6 Months">6 Months</option>
+                    <option value="1 Year">1 Year</option>
+                    <option value="2 Years">2 Years</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="buttons-container">
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+              <button
+                type="reset"
+                className="cancel-button"
+                onClick={() => {
+                  navigate("/dashboard");
+                }}
+              >
+                Cancel
+              </button>
             </div>
           </Form>
         </Formik>
